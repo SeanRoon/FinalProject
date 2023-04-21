b7d1efe64e1a1a47fdf6db66b90db60f8383af5d
rooneys24 <rooneysw@gmail.com>
2023-04-21 10:37:13 -0400

    testing

---
+++ app/src/main/java/com/example/finalproject/FirebaseFragment.kt
@@ -0,0 +1,24 @@
+package com.example.finalproject
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
+class FirebaseFragment : Fragment() {
+
+    lateinit var dbRef : DatabaseReference
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val rootview = inflater.inflate(R.layout.fragment_firebase, container, false)
+        dbRef = Firebase.database.reference
+        return rootview
+    }
+}
