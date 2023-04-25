30a381585842681b258d11ac50fcd39aa22cbed2
rooneys24 <rooneysw@gmail.com>
2023-04-25 10:37:22 -0400

    2

--- app/src/main/java/com/example/finalproject/FirebaseFragment.kt
+++ app/src/main/java/com/example/finalproject/FirebaseFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.finalproject.databinding.FragmentFirebaseBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
@@ -12,13 +15,27 @@ import com.google.firebase.ktx.Firebase
 class FirebaseFragment : Fragment() {
 
     lateinit var dbRef : DatabaseReference
+    lateinit var fAuth: FirebaseAuth
+    private var _binding: FragmentFirebaseBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val rootview = inflater.inflate(R.layout.fragment_firebase, container, false)
+        _binding = FragmentFirebaseBinding.inflate(inflater, container, false)
+        val rootView = binding.root
         dbRef = Firebase.database.reference
-        return rootview
+        fAuth = Firebase.auth
+        val username = binding.editTextTextEmailAddress.text.toString()
+        binding.registerButton.setOnClickListener(){
+            firebaseSignUp(username, )
+        }
+
+        return rootView
+    }
+
+    fun firebaseSignUp(){
+        fAuth.createUserWithEmailAndPassword()
     }
 }
--- app/src/main/java/com/example/finalproject/MainActivity.kt
+++ app/src/main/java/com/example/finalproject/MainActivity.kt
@@ -3,8 +3,11 @@ package com.example.finalproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.example.finalproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
