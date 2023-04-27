458b3a1fbf32709aecb05f264b36dc2dfd2d988f
rooneys24 <rooneysw@gmail.com>
2023-04-27 10:38:28 -0400

    testing

--- app/src/main/java/com/example/finalproject/LoginFragment.kt
+++ app/src/main/java/com/example/finalproject/LoginFragment.kt
@@ -5,32 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.finalproject.databinding.FragmentFirebaseBinding
+import com.example.finalproject.databinding.FragmentLoginBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 class LoginFragment : Fragment() {
 
-    lateinit var dbRef : DatabaseReference
-    lateinit var fAuth: FirebaseAuth
-    private var _binding: FragmentFirebaseBinding? = null
+    private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentFirebaseBinding.inflate(inflater, container, false)
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
         val rootView = binding.root
-        dbRef = Firebase.database.reference
-        fAuth = Firebase.auth
-        val username = binding.editTextTextEmailAddress.text.toString()
-        binding.registerButton.setOnClickListener(){
-
-        }
 
         return rootView
     }
