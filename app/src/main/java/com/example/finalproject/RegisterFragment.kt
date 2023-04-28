5bcd3c6709208c1f13ae9cde66fdff4f52d86429
rooneys24 <rooneysw@gmail.com>
2023-04-28 10:37:58 -0400

    testing

--- app/src/main/java/com/example/finalproject/RegisterFragment.kt
+++ app/src/main/java/com/example/finalproject/RegisterFragment.kt
@@ -1,31 +1,53 @@
 package com.example.finalproject
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
 import com.example.finalproject.databinding.FragmentRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class RegisterFragment : Fragment() {
     private var _binding: FragmentRegisterBinding? = null
     private val binding get() = _binding!!
-//    lateinit var editTextEmail: EditText
-//    lateinit var editTextPassword: EditText
-//    lateinit var buttonReg: Button
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val rootView = inflater.inflate(R.layout.fragment_register, container, false)
-
+        mAuth = Firebase.auth
         binding.btnRegister.setOnClickListener(){
-            val email = binding.email.text.toString()
-        }
+            binding.progressBar.visibility = View.VISIBLE
+            val email = binding.emailText.text.toString()
+            val password = binding.passwordText.text.toString()
 
+            if (binding.emailText.text.toString().equals("")){
+                Toast.makeText(activity, "Enter email", Toast.LENGTH_SHORT).show()
+            }
+            if (binding.passwordText.text.toString().equals("")){
+                Toast.makeText(activity, "Enter password", Toast.LENGTH_SHORT).show()
+            }
+            mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener() { task ->
+                    binding.progressBar.visibility = View.GONE
+                    if (task.isSuccessful) {
+                        Toast.makeText(activity, "Account created:", Toast.LENGTH_SHORT,).show()
+                    } else {
+                        Toast.makeText(activity, "Authentication failed.", Toast.LENGTH_SHORT,).show()
+                    }
+                }
+        }
         return rootView
     }
 }
