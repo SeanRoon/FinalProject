dee8f9be55238ab217ca3f4b82b5be2379f020d8
rooneys24 <rooneysw@gmail.com>
2023-05-08 10:37:47 -0400

    testing

--- app/src/main/java/com/example/finalproject/AddFoodFragment.kt
+++ app/src/main/java/com/example/finalproject/AddFoodFragment.kt
@@ -5,13 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.example.finalproject.databinding.FragmentAddFoodBinding
 
 class AddFoodFragment : Fragment() {
+    private var _binding: FragmentAddFoodBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_food, container, false)
+        _binding = FragmentAddFoodBinding.inflate(inflater, container, false)
+        val rootView = binding.root
+        binding.submitButton.setOnClickListener(){
+            val food1 = binding.caloriesFood.text.toString()
+            val cal = binding.caloriesNumber.text.toString()
+            Toast.makeText(activity, food1 + cal, Toast.LENGTH_LONG).show()
+        }
+        return rootView
     }
 }
---
+++ app/src/main/java/com/example/finalproject/Food.java
@@ -0,0 +1,6 @@
+package com.example.finalproject;
+
+import java.io.Serializable;
+
+public class Food implements Serializable {
+}
